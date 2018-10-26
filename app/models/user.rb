7c7005754cd0ad323e385af6cfb385b8e568e75d
EC2 Default User <ec2-user@ip-172-31-45-102.ap-southeast-1.compute.internal>
2018-10-26 10:22:01 +0000

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -55,16 +55,19 @@ class User < ApplicationRecord
   # パスワード再設定の属性を設定する
   def create_reset_digest
     self.reset_token = User.new_token
-    # update_attribute(:reset_digest,  User.digest(reset_token))
-    # update_attribute(:reset_sent_at, Time.zone.now)
-    update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
+    update_columns(reset_digest:  User.digest(reset_token), reset_sent_at: Time.zone.now)
   end
 
   # パスワード再設定のメールを送信する
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
   end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
   
+  # パスワード再設定の期限が切れている場合はtrueを返す  
   private
   # メールアドレスをすべて小文字にする
   def downcase_email
@@ -77,8 +80,4 @@ class User < ApplicationRecord
     self.activation_digest = User.digest(activation_token)
   end
   
-  # パスワード再設定の期限が切れている場合はtrueを返す
-  def password_reset_expired?
-    reset_sent_at < 2.hours.ago
-  end
 end
